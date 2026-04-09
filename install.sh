7f49e3e3564c4d69267f1b612c99cf8b7a7ab9b4
tauzn-clock <tauzn-clock@github.com>
2026-04-09 11:40:32 +0200

    install using pip instead of uv

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-uv sync
-uv run maturin develop --release --uv
-uv pip install torch-cluster==1.6.3 -f https://data.pyg.org/whl/torch-2.7.0+cu126.html --no-build-isolation
-uv pip install torch-spline-conv==1.2.2 -f https://data.pyg.org/whl/torch-2.7.0+cu126.html --no-build-isolation
+pip install -e . --no-build-isolation
+maturin develop --release
+pip install torch-cluster==1.6.3 -f https://data.pyg.org/whl/torch-2.7.0+cu126.html --no-build-isolation
+pip install torch-spline-conv==1.2.2 -f https://data.pyg.org/whl/torch-2.7.0+cu126.html --no-build-isolation
